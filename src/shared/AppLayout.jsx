775fb43177e7644b74260a3ccbe7101f447adb70
matrojadharmraj486-cmd <matrojadharmraj486@gmail.com>
2026-03-22 21:53:37 +0530

    Add space

--- src/shared/AppLayout.jsx
+++ src/shared/AppLayout.jsx
@@ -9,6 +9,7 @@ export function AppLayout() {
     <div className="min-h-screen bg-gray-50 text-gray-900 dark:bg-gray-900 dark:text-gray-100">
       <Topbar onMenu={() => setOpen((v) => !v)} />
       <div className="flex">
+        
         <Sidebar open={open} onClose={() => setOpen(false)} />
         <main className="flex-1 p-4 md:p-6 lg:p-8">
           <Outlet />
