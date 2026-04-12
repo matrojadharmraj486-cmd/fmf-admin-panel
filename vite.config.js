cf72dfbe29f2724b408c866c5984611392fbc60d
matrojadharmraj486-cmd <matrojadharmraj486@gmail.com>
2026-04-12 22:05:26 +0530

    changes in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/FMF-Admin/',
+  base: '/',
   assetsInclude: ['**/*.xlsx'],
   server: {
     port: 5173,
